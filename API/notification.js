1554840b4751255e20936d3a4ca46408d1eb64ae
Rajeev Joshi <rjoshi@Rajeevs-Mac.local>
2013-04-03 19:41:54 -0400

    Initial Checkin

---
+++ API/notification.js
@@ -0,0 +1,32 @@
+// api-notification
+var showAlert = function() {
+    function alertDismissed() {
+        console.log("Alert dismissed");
+    }
+    navigator.notification.alert(
+        'You are the winner!',  // message
+        alertDismissed,         // callback
+        'Game Over',            // title
+        'Done'                  // buttonName
+    );    
+};
+var showConfirm = function() {
+    function onConfirm(button) {
+        alert('You selected button ' + button);
+    }
+    navigator.notification.confirm(
+            'You are the winner!',  // message
+            onConfirm,              // callback to invoke with index of button pressed
+            'Game Over',            // title
+            'Restart,Exit'          // buttonLabels
+        );    
+};
+
+//iOS Quirk - Ignores duration and plays beep.wav which must be located in the www folder.
+var beep = function() {
+    navigator.notification.beep(2);
+};
+
+var vibrate = function() {
+    navigator.notification.vibrate(2000);
+};
---
+++ index.js
@@ -0,0 +1,42 @@
+$('#page-home').live('pageinit', function(event){  
+    $('.api-div').hide();
+    $('.api-div#api-intro').show();
+    
+    $('#intro').click(function() {
+        $('.api-div').hide();
+        $('.api-div#api-intro').show();
+        $.mobile.silentScroll(0);            
+    });
+    
+    $('div ul li a').click(function(event) {
+        event.preventDefault();
+        //alert('clicked : ' + $(this).attr('id'));
+        var attrId = $(this).attr('id');
+
+        if (attrId.indexOf("click") !== 0) {
+            return;
+        }
+        
+        var api = '#api' + attrId.substring(attrId.indexOf('-'));
+        
+        // hide all div's, show only this one
+        $('.api-div').hide();
+        $(api).show();
+
+        // if small screen and portrait - close after tap
+        var disp = $('ul #listdivider').css("display");
+        //alert(disp + ' : ' + api);
+        if (disp === 'none') {
+            $('div.ui-collapsible').trigger("collapse");
+        } else {
+            $.mobile.silentScroll(0);            
+        }
+    }); 
+    
+    $('#listdivider').click(function(event) {
+        event.preventDefault();
+        $('.api-div').hide();
+        $('.api-div#api-intro').show();
+        $.mobile.silentScroll(0);
+    });
+});
